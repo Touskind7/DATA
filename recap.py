9f950b90a3e1aaa95df900b3771e597988d94c72
Toussaint <toussainttovitchede@gmail.com>
2024-05-29 19:34:06 +0100

    fichier python

--- recap.py
+++ recap.py
@@ -1,4 +1,28 @@
 #
+#
+
+#while x < 100:
+    #x += 1 
+    #print(x)
+    #break
+
+#liste = ["Janvier", "Février", "Mars", "Avril", "Mai"]
+
+#for index, mois in enumerate(liste):
+     #print(index, mois)
+    # if index !=2:
+   #    print(index, mois)
+  #     break
+
+#while index  !=0:
+ #   print()
+
+
+x_A = 2
+y_A = 5
+x_B = 2
+y_B = 1
+d = distance(x_A, y_A, x_B, y_B)
 
 #ajouter 
 
